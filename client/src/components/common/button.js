86135b4653dc69add605106bf81c61f057ea19d1
Anton Romankov <romant094@gmail.com>
2019-01-21 22:11:03 +0300

    Moved http requests to one place

--- client/src/components/common/button.js
+++ client/src/components/common/button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { postBudget,postTransaction } from '../common/routes';
 
 class Button extends React.Component {
     constructor(props) {
@@ -12,7 +13,7 @@ class Button extends React.Component {
 
         this.chooseAction = this.chooseAction.bind(this);
         this.setBudget = this.setBudget.bind(this);
-        this.postTransaction = this.postTransaction.bind(this);
+        this.sendTransaction = this.sendTransaction.bind(this);
     }
 
     chooseAction(param) {
@@ -27,30 +28,16 @@ class Button extends React.Component {
     setBudget() {
         const budget = +document.getElementById(this.state.fieldID).value,
             json = JSON.stringify({ budget: budget });
-        fetch('http://localhost:8000/budget', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: json
-        });
+        postBudget(json);
     }
 
-    postTransaction() {
+    sendTransaction() {
         const json = JSON.stringify({
             type: this.state.action,
             sum: +document.getElementById(this.state.fieldID).value,
             date: new Date().toISOString().substring(0, 10)
         })
-        fetch('http://localhost:8000/transaction', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json'
-            },
-            body: json
-        });
+        postTransaction(json);
     }
 
     render() {
