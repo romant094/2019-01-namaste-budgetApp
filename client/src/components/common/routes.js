4d597190207feb36c09d722b1eabf8c11223891d
Anton Romankov <romant094@gmail.com>
2019-01-22 18:20:23 +0300

    Trying to use routes from separate file

--- client/src/components/common/routes.js
+++ client/src/components/common/routes.js
@@ -1,9 +1,46 @@
-function GetTransactions() {
+function getTransactions() {
     fetch('http://localhost:8000/transactions')
         .then(response => response.json())
         .then(response => {
-            return response;
+            console.log(response)
         });
 }
 
-export default GetTransactions;
+function postTransaction(json) {
+    fetch('http://localhost:8000/transaction', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: json
+    });
+}
+
+function deleteTransaction(id) {
+    fetch('http://localhost:8000/transaction', {
+        method: 'DELETE',
+        params: {
+            id: id
+        }
+    });
+}
+
+function postBudget(json) {
+    fetch('http://localhost:8000/budget', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: json
+    });
+}
+
+function getBudget() {
+    fetch('http://localhost:8000/budget')
+        .then(response => response.json());
+}
+
+export default getTransactions;
+export { postBudget, getBudget, postTransaction, deleteTransaction };
